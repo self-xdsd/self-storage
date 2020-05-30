d89adc36b80ffe95e9f208ea154113b6d9f06e77
amihaiemil <amihaiemil@gmail.com>
2020-05-30 17:22:17 +0300

    JOOQ scaffolding

---
+++ src/main/java/com/selfxdsd/storage/SelfJooq.java
@@ -0,0 +1,82 @@
+/**
+ * Copyright (c) 2020, Self XDSD Contributors
+ * All rights reserved.
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"),
+ * to read the Software only. Permission is hereby NOT GRANTED to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software.
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+ * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+ * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.selfxdsd.storage;
+
+import com.selfxdsd.api.*;
+import com.selfxdsd.api.storage.Storage;
+
+/**
+ * Self Storage implemented with jOOQ.
+ * @author Mihai Andronache (amihaiemil@gmail.com)
+ * @version $Id$
+ * @since 0.0.1
+ */
+public final class SelfJooq implements Storage {
+
+    private Database database;
+
+    /**
+     * Constructor. Working with MySql by default.
+     */
+    public SelfJooq() {
+        this(new MySql());
+    }
+
+    /**
+     * Ctor.
+     * @param database Database.
+     */
+    public SelfJooq(final Database database) {
+        this.database = database;
+    }
+
+    @Override
+    public Users users() {
+        return new SelfUsers(this.database);
+    }
+
+    @Override
+    public ProjectManagers projectManagers() {
+        return null;
+    }
+
+    @Override
+    public Projects projects() {
+        return null;
+    }
+
+    @Override
+    public Contracts contracts() {
+        return null;
+    }
+
+    @Override
+    public Contributors contributors() {
+        return null;
+    }
+
+    @Override
+    public Tasks tasks() {
+        return null;
+    }
+}
